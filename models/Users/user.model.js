1a02afcf700dd7cab7bee9a3ebc05ba4afe13d48
Oscar Jovanny García Zepeda <oscarin962011@hotmail.com>
2019-01-04 14:17:26 -0800

    User model created.

---
+++ models/Users/user.model.js
@@ -0,0 +1,40 @@
+const User = require('./../admin.models').User;
+
+User.getAllUsers = ( res, cb ) => {
+  User.findAll()
+  .then( data => cb( null, res, data, 200 ) )
+  .catch( error => cb( error, res ) )
+}
+
+User.getSingleUser = ( id, res, cb ) => {
+  User.findByPk(id)
+  .then( data => cb( null, res, data, 200 ) )
+  .catch( error => cb( error, res ) )
+}
+
+User.saveNewUser = ( newUser, res, cb ) => {
+  User.create(newUser)
+  .then( data => cb( null, res, data, 200 ) )
+  .catch( error => cb( error, res ) )
+}
+
+User.updateUser = ( user, res, cb ) => {
+  User.update(user, { where: { username: user.username } } )
+  .then( data => cb( null, res, data, 200 ) )
+  .catch( error => cb( error, res ) )
+}
+
+User.deleteUser = ( idUser, res, cb ) => {
+  User.update({ statusItem: 1 }, { where: { id: idUser } })
+  .then( data => cb( null, res, data, 200 ) )
+  .catch( error => cb( error, res ) )
+}
+
+User.responseToClient = ( error, res, data, status ) => {
+  if ( error )
+    res.status(500).json(error);
+  else
+    res.status(status).json(data);
+}
+
+module.exports = User;
---
+++ models/Users/user.routes.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const router = express.Router();
+
+const User = require('./user.model');
+
+router.get('/', (req, res) => {
+  return User.getAllUsers( res, User.responseToClient )
+})
+
+router.get('/:id', (req, res) => {
+  let id = req.params.id;
+  return User.getSingleUser( id, res, User.responseToClient )
+})
+
+router.post('/', ( req, res ) => {
+  let user = {
+    username: req.body.username,
+    name: req.body.name,
+    email: req.body.email,
+    phoneNumber: req.body.phoneNumber,
+    password: req.body.password,
+    statusItem: 0
+  }
+
+  return User.saveNewUser( user, res, User.responseToClient );
+})
+
+router.put('/', (req, res) => {
+  let user = {
+    username: req.body.username,
+    name: req.body.name,
+    phoneNumber: req.body.phoneNumber,
+    password: req.body.password
+  }
+
+  return User.updateUser( user, res, User.responseToClient );
+})
+
+router.delete('/:id', (req, res) => {
+  let id = req.params.id;
+  return User.deleteUser( id, res, User.responseToClient );
+})
+
+module.exports = router;
