49a3ae5a4c1df8f2ba53e3317df7a781c89a6363
KarinGarciaZ <oscarin962011@hotmail.com>
2019-01-05 16:58:19 -0600

    File's name changed.

--- models/admin.routes.js
+++ models/admin.routes.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-//const Image = require('./Images/image.routes');
-//const Construction = require('./Constructions/construction.routes');
+const Image = require('./Images/image.routes');
+const Construction = require('./Constructions/construction.routes');
 const Type = require('./Types/type.routes');
 const User = require('./Users/user.routes');
 
 router.use('/type', Type);
 
-//router.use('/construction', Construction);
+router.use('/construction', Construction);
 
-//router.use('/image', Image);
+router.use('/image', Image);
 
 router.use('/user', User);
 
