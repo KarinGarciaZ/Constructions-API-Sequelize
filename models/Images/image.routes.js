56054bce5ed4847f6556afd6436fc729b813dca5
KarinGarciaZ <oscarin962011@hotmail.com>
2019-01-05 17:00:52 -0600

    New implementation with model Image.

--- models/Images/image.routes.js
+++ models/Images/image.routes.js
@@ -7,20 +7,4 @@ router
   return Image.getAllImages( res, Image.responseToClient );
 } )
 
-.post( '/', ( req, res ) => {
-  const img = {
-    id_image: null,
-    id_Constructions: req.body.id_Constructions,
-    url: req.body.url
-  }
-
-  return Image.saveImage( img, res, Image.responseToClient );
-})
-
-.delete( '/:idImage', ( req, res ) => {
-  let idImage = req.params.idImage;
-
-  return Image.deleteImage( idImage, res, Image.responseToClient );
-})
-
 module.exports = router;
