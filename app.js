d44bfbd83098b24ece6174e87ab207063880c010
KarinGarciaZ <oscarin962011@hotmail.com>
2019-01-20 14:56:39 -0600

    Sessions removed.

--- app.js
+++ app.js
@@ -5,38 +5,22 @@
   const bodyParser = require('body-parser');
   const cors = require('cors');
   const env = require('dotenv');
-  const session = require('express-session');
-  var MySQLStore = require('express-mysql-session')(session);
   env.config();
-
-  const adminRoutes = require('./models/admin.routes');
 
   app.use(bodyParser.json());
 
-  var options = {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
-  };
-  
-  var sessionStore = new MySQLStore(options);
-  
-  app.use(session({
-      secret: 'session_cookie_secret',
-      store: sessionStore,
-      resave: false,
-      saveUninitialized: false
-  }));
-
   app.use(cors({ origin:true, credentials: true }));
+
+  const Authentication = require('./models/Authentication/authentication.routes');
+  app.use('/auth', Authentication)
   
-  app.use(adminRoutes);
+  const adminRoutes = require('./models/admin.routes');
+  const isAuth = require('./auth/auth');
+  app.use( isAuth, adminRoutes);  
 
   const environmentConfig = require('./enviroments');
 
-  app.listen(environmentConfig.port, environmentConfig.host, () => {
+  app.listen(3001, environmentConfig.host, () => {
     console.log(`API ready to get requests...`);
     console.log('running on port ' , environmentConfig.port);
   });
