e0e2504b6ea028ee43581ad306b06ee486cf334a
Oscar Jovanny García Zepeda <oscarin962011@hotmail.com>
2019-01-22 09:10:21 -0800

    Added new access-control-allow-origin.

--- app.js
+++ app.js
@@ -9,6 +9,14 @@
 
   app.use(bodyParser.json());
 
+  app.use( ( req, res, next ) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+    next();
+  } )
+
   app.use(cors());
 
   const Authentication = require('./models/Authentication/authentication.routes');
