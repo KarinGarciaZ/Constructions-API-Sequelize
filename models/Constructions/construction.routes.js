c9809f6a535bd1fa86059adff9a6ff77082b62f7
Oscar Jovanny García Zepeda <oscarin962011@hotmail.com>
2019-02-01 15:43:18 -0800

    Multer config made in a different module.

--- models/Constructions/construction.routes.js
+++ models/Constructions/construction.routes.js
@@ -2,26 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Construction = require('./construction.model');
 const isAuth = require('../../auth/userAuth');
-
-const multer = require('multer');
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, './storage/constructions/');
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + '-' + file.originalname );
-  }
-});
-
-const fileFilter = (req, file, cb) => {
-  if( file.mimetype === 'image/jpeg' || file.mimetype === 'image/png' )
-    cb(null, true);
-  else
-    cb(null, false);
-}
-
-const upload = multer({storage: storage, fileFilter: fileFilter}).array('image', 30);
+const upload = require( './multer-configuration' );
 
 router
 
---
+++ models/Constructions/multer-configuration.js
@@ -0,0 +1,21 @@
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, './storage/constructions/');
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + '-' + file.originalname );
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  if( file.mimetype === 'image/jpeg' || file.mimetype === 'image/png' )
+    cb(null, true);
+  else
+    cb(null, false);
+}
+
+const upload = multer({storage: storage, fileFilter: fileFilter}).array('image', 30);
+
+module.exports = upload;
