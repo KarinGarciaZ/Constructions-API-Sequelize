70d6a9f36a33f28f87d7cb2a1e5660f15b0d3f24
Oscar Jovanny García Zepeda <oscarin962011@hotmail.com>
2019-02-20 09:27:14 -0800

    Endpoint added to validate code and change password.

--- models/ResetCode/resetCode.routes.js
+++ models/ResetCode/resetCode.routes.js
@@ -6,7 +6,15 @@ const ResetCode = require('./resetCode.model');
 router.post('/resetPassword', ( req, res ) => {
   let email = req.body.email;
 
-  return ResetCode.resetPassword( email, res, ResetCode.responseToClient );
+  return ResetCode.sendEmail( email, res, ResetCode.responseToClient );
+})
+
+router.post('/verifyCode', ( req, res ) => {
+  let code = req.body.code;
+  let userId = req.body.userId;
+  let newPassword = req.body.password;
+
+  return ResetCode.verifyCode( code, userId, newPassword, res, ResetCode.responseToClient );
 })
 
 module.exports = router;
