5616d4d64dbf855a9be328489e1fba1723f025f3
Oscar Jovanny García Zepeda <oscarin962011@hotmail.com>
2019-02-25 16:19:03 -0800

    Added and identifier to errors.

--- auth/auth.js
+++ auth/auth.js
@@ -3,13 +3,13 @@ const jwt = require('jsonwebtoken');
 module.exports = ( req, res, next ) => {
   let token = req.headers['authorization'];
   if ( typeof(token) === 'undefined' ) 
-    return res.status(403).json('You cannot hit this endpoint.');
+    return res.status(403).json('You cannot hit this endpoint4.');
 
   let bearer = token.split(' ');
   token = bearer[1];
   
   jwt.verify( token, process.env.SECRET_KEY, err => {
-    if(err) return res.status(403).json('You cannot hit this endpoint.');
+    if(err) return res.status(403).json('You cannot hit this endpoint5.');
     
     next();
   })  
--- auth/userAuth.js
+++ auth/userAuth.js
@@ -3,16 +3,16 @@ const jwt = require('jsonwebtoken');
 module.exports = ( req, res, next ) => {
   let token = req.headers['authorization'];
   if ( typeof(token) === 'undefined' ) 
-    return res.status(403).json('You cannot hit this endpoint.');
+    return res.status(403).json('You cannot hit this endpoint1.');
 
   let bearer = token.split(' ');
   token = bearer[1];
 
   jwt.verify( token, process.env.SECRET_KEY, ( err, info) => {
-    if(err) return res.status(403).json('You cannot hit this endpoint.');
+    if(err) return res.status(403).json('You cannot hit this endpoint2.');
     if( info.userId )//falta verificar si el usuario existe
       next();
     else
-      return res.status(403).json('You cannot hit this endpoint.');
+      return res.status(403).json('You cannot hit this endpoint3.');
   })  
 }
