b005eb743f9346b149b62296c712314121bddc9d
Oscar Jovanny García Zepeda <oscarin962011@hotmail.com>
2019-02-26 14:38:46 -0800

    User validation added to auth.

--- auth/userAuth.js
+++ auth/userAuth.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const User = require('../models/admin.models').User;
 
 module.exports = ( req, res, next ) => {
   let token = req.headers['authorization'];
@@ -10,9 +11,17 @@ module.exports = ( req, res, next ) => {
 
   jwt.verify( token, process.env.SECRET_KEY, ( err, info) => {
     if(err) return res.status(403).json('You cannot hit this endpoint2.');
-    if( info.userId )//falta verificar si el usuario existe
-      next();
+    if( info.userId ) {
+      User.findByPk( info.userId )
+      .then( value => {
+        if (!value.statusItem)
+          next();
+        else
+          return res.status(403).json('You cannot hit this endpoint3.');
+      })
+      .catch( error => res.status(403).json('You cannot hit this endpoint4.') )
+    }
     else
-      return res.status(403).json('You cannot hit this endpoint3.');
+      return res.status(403).json('You cannot hit this endpoint5.');
   })  
 }
