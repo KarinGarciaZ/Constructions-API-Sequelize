24aedbc2b84ce28e2fd00e745b4a2915073c78e3
Oscar Jovanny García Zepeda <oscarin962011@hotmail.com>
2019-04-22 19:42:19 -0500

    Changed cors implementation.

--- app.js
+++ app.js
@@ -9,27 +9,25 @@
   env.config();
 
   app.use(bodyParser.json({limit: '50mb'}));
+  
+  app.use( ( req, res, next ) => {    
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Headers", "Authorization, Access-Control-Allow-Headers, Content-Type");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS ")
+    next();
+  })
 
-  // app.use( ( req, res, next ) => {
-  //   console.log(req.headers['authorization'])
-  //   res.setHeader('Access-Control-Allow-Origin', '*');
-  //   res.setHeader('Access-Control-Allow-Headers', 'Authorization, Content-Type');
-  //   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
-  //   next();
-  // })
-
-  // app.use( ( req, res, next ) => {
-  //   if ( req.method === 'OPTIONS' ) {
-  //     res.sendStatus(204);
-  //   }
-  //   next();
-  // })
+  app.use( ( req, res, next ) => {
+    if ( req.method === 'OPTIONS' ) {
+      res.sendStatus(204);
+    }
+    next();
+  })
 
   //app.use( cors({ credentials: true, origin: true }) )
 
-  app.use(cors( {credentials: true,  origin: '*', methods: 'GET, POST, PUT, DELETE, OPTIONS', allowedHeaders: 'Content-Type, Authorization'} ));
-
-  //app.use(formData.parse());
+  //app.use(cors( {credentials: true,  origin: '*', methods: 'GET, POST, PUT, DELETE, OPTIONS', allowedHeaders: 'Content-Type, Authorization'} ));
 
   app.use(require('./default_values/user'));
 
